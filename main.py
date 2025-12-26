d6389451aa86cda3aa30daae3d9b05cc4aaddaa0
SleepyCaTT1425 <pannawitchakorn14@gmail.com>
2025-12-26 14:32:45 +0700

    change to use function index

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!sss</p>"
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello, World!sss</p>"
 
-@app.route('/hello/')
-def hello_world1():
-    return 'Hi my name is someone'
+# @app.route('/hello/')
+# def hello_world2():
+#     return 'Hi my name is someone'
+
+@app.route("/")
+def index():
+    # This function runs when you visit http://localhost:5000/
+    return render_template('index.html')
